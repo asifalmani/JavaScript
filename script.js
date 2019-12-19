d4a0860ae7e4b4c35ae310f0ad41d77934c5bfa8
asifalmani <asif.shahzad@coeus-solutions.de>
2019-12-19 20:04:28 +0500

    Operator Precedence done

--- script.js
+++ script.js
@@ -31,7 +31,7 @@ var johanYear = year -18;
 console.log(johanYear);*/
 
 // Math Operators
-var year, yearJohan, yearMark;
+/*var year, yearJohan, yearMark;
 now = 2019;
 ageJohn = 28;
 ageMark = 33;
@@ -52,4 +52,45 @@ console.log(johnOlder);
 // Type of Operators
 console.log(typeof johnOlder);
 console.log(typeof ageMark);
-console.log(typeof now);
+console.log(typeof now);*/
+
+
+/*---------------------------------
+	Operator Precedence
+*/
+
+var now = 2019;
+var yearJohan = 1989;
+var fullAge = 18;
+
+// Multiple Operators
+var isfullAge = now - yearJohan >= fullAge;
+console.log(isfullAge);
+
+// Grouping
+var ageJohn = now - yearJohan;
+var ageMark = 35;
+var average = (ageJohn + ageMark) / 2;
+console.log(average);
+
+// Multiple Assignments
+var x, y;
+x = (3+5) * 4 - 6; /*8 *4 -6 // 32-6 //26*/
+x = y = (3+5) * 4 - 6; /*8 *4 -6 // 32-6 //26*/
+console.log(x);
+console.log(x, y);
+
+// More Operators
+x *=2;
+console.log(x);
+x += 10;
+console.log(x);
+/*x = x + 1;
+x += 1;*/
+x++;
+console.log(x);
+x--;
+console.log(x);
+
+
+https://www.udemy.com/course/the-complete-javascript-course/learn/lecture/10788388#overview
