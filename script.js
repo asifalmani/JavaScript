ca05895a2264d6822cf0b64c78eddd52a1903f54
asifalmani <asif.shahzad@coeus-solutions.de>
2019-12-19 19:39:02 +0500

    starter template added

---
+++ script.js
@@ -0,0 +1,55 @@
+/*---------------------------------
+	Variables and Data Types
+*/
+
+/* var firstName = 'Muhammad';
+ console.log(firstName);
+
+ var middleName = 'Asif';
+ var lastName = 'Shahzad';
+ var age = 37;
+ var job = 'Front End Developer';
+
+// Variables nameing rules
+var _3years = 3;
+var johanMark = 'Johan and Mark'
+var if = 23;*/
+
+
+
+// Variables mutation and type correction 
+
+
+/*---------------------------------
+	Basic Operators
+*/
+/*var year = 2018;
+var johanYear = 2018 - 20;
+var markYear = 2018-33;
+
+var johanYear = year -18;
+console.log(johanYear);*/
+
+// Math Operators
+var year, yearJohan, yearMark;
+now = 2019;
+ageJohn = 28;
+ageMark = 33;
+
+var johanYear = now - ageJohn;
+var yearMark = now-ageJohn;
+
+console.log(johanYear);
+console.log(now + 2);
+console.log(now * 2);
+console.log(now / 10);
+
+
+// Logical Operators
+var johnOlder = ageJohn < ageMark;
+console.log(johnOlder);
+
+// Type of Operators
+console.log(typeof johnOlder);
+console.log(typeof ageMark);
+console.log(typeof now);
