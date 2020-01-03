53c41f3f1791e4bb3d634ac76cd1707d85f7e58c
asifalmani <asif.shahzad@coeus-solutions.de>
2020-01-03 11:38:26 +0500

    array topic covered

--- script.js
+++ script.js
@@ -320,8 +320,32 @@ console.log(whatDoYouDo('designer', 'Asif'));*/
 	* Arrays
 */
 
+// Initialize new array
 var names = ['John', 'Mark', 'Jane'];
 var years = new Array(1990, 1969, 1948);
 
 console.log(names[2]);
 console.log(names.length);
+
+// Muted array data
+names[1] = 'Ben';
+names[names.length] = 'Mary'
+console.log(names);
+
+// Different data types
+var john = ['John', 'Smith', 1990, 'designer', false];
+
+john.push('blue');
+john.unshift('Mr.');
+console.log(john);
+
+john.pop();
+john.pop();
+john.shift();
+console.log(john);
+
+console.log(john.indexOf(23));
+
+var isDesigner = john.indexOf('designer') === -1 ? 'John is Not a designer' : 'John IS a designer';
+console.log(isDesigner);
+
